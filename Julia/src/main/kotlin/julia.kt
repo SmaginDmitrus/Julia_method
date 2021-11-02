580ac5e4e17725885651bf93d87a21638aa684a7
SmaginDmitrus <74429578+SmaginDmitrus@users.noreply.github.com>
2021-11-02 19:08:18 +0300

    Update julia.kt

--- Julia/src/main/kotlin/julia.kt
+++ Julia/src/main/kotlin/julia.kt
@@ -6,13 +6,13 @@ import jetbrains.letsPlot.geom.*
 import jetbrains.letsPlot.letsPlot
 
 
-fun <A,B,C> compose(f:(B)->C,g:(A)->B):(A)->C = {x -> f(g(x))}
+fun <A,B,C> compose(f:(B)->C,g:(A)->B):(A)->C = {x -> f(g(x))} //создаём шаблон функции, которая создаёт композицию функций
 
-fun apply(f:(Complex)->Complex,n:Int):(Complex)->Complex {
+fun apply(f:(Complex)->Complex,n:Int):(Complex)->Complex { //создаём функцию, которая применяет функцию саму на себя n раз
  if (n==0){ return f} else { return compose(apply(f,n-1),f)}
 }
 
-fun f(z:Complex):Complex{
+fun f(z:Complex):Complex{ // определяем функцию, которая будет изменять комплексное число
  val z1 = -0.4 + 0.6* I
  return z*z + z1
 }
@@ -28,15 +28,8 @@ fun f(z:Complex):Complex{
   var ys = arrayListOf<Int>()
   var value = arrayListOf<Int>()
   var tempMap: Array<Array<Int>> = Array(400, { Array(400,){0} })
-  var numMap: Array<Array<Int>> = Array(400, { Array(400,){0} })
+  
 
-  for (i in 0..399)
-  {
-   for (j in 0..399)
-   {
-    numMap[i][j] = j + 1
-   }
-  }
 
   for (i in 0..399)
   {
@@ -48,11 +41,11 @@ fun f(z:Complex):Complex{
     while (flag==true)
     {
      sum = apply(::f,n)(z)
-     if(n == 150){
+     if(n == 150){//задаём лимит на n, чтобы цикл не ушёл в бесконечность при определённых значениях z
       tempMap[i][j] = 150
       flag = false
      }
-     if(sum.mod > 2.0){
+     if(sum.mod > 2.0){// если модуль получившегося комплексного числа больше двойки, останавливаем цикл и запоминаем n
       flag = false
       tempMap[i][j]=n-1
      }
@@ -61,18 +54,11 @@ fun f(z:Complex):Complex{
    }
   }
 
-  for (i in 0..399)
-  {
-   for (j in 0..399){
-    print(tempMap[i][j])
-    print(" ")
-   }
-   println()
-  }
 
-  for (i in 0..399)
+
+  for (i in 0..399)//создаём массивы координат точек и значений
   {
-   for (j in 0..399){
+   for (j in 0..399){ 
     xs.add(i)
     ys.add(j)
     value.add(tempMap[i][j])
@@ -80,8 +66,8 @@ fun f(z:Complex):Complex{
   }
 
   val data_3 = mapOf<String, Any>("xs" to xs,"ys" to ys , "zs" to value)
-  val fig_3 = letsPlot(data_3) +
-          // something wring with `geomRaster'?
+  val fig_3 = letsPlot(data_3) + 
+          
           geomTile() {x = "xs"; y = "ys"; fill = "zs"}
 
   ggsave(fig_3, "plot_3.png")
