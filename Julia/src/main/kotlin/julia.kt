784a4b339cfc68cab2835c13823f6933b2b866d3
SmaginDmitrus <74429578+SmaginDmitrus@users.noreply.github.com>
2021-11-02 23:14:05 +0300

    Update julia.kt

--- Julia/src/main/kotlin/julia.kt
+++ Julia/src/main/kotlin/julia.kt
@@ -3,17 +3,17 @@ import org.kotlinmath.*
 
 import jetbrains.letsPlot.export.ggsave
 import jetbrains.letsPlot.geom.*
-import jetbrains.letsPlot.letsPlot
 import jetbrains.letsPlot.label.labs
+import jetbrains.letsPlot.letsPlot
 
 
-fun <A,B,C> compose(f:(B)->C,g:(A)->B):(A)->C = {x -> f(g(x))} //создаём шаблон функции, которая создаёт композицию функций
+fun <A,B,C> compose(f:(B)->C,g:(A)->B):(A)->C = {x -> f(g(x))}
 
-fun apply(f:(Complex)->Complex,n:Int):(Complex)->Complex { //создаём функцию, которая применяет функцию саму на себя n раз
+fun apply(f:(Complex)->Complex,n:Int):(Complex)->Complex {
  if (n==0){ return f} else { return compose(apply(f,n-1),f)}
 }
 
-fun f(z:Complex):Complex{ // определяем функцию, которая будет изменять комплексное число
+fun f(z:Complex):Complex{
  val z1 = -0.4 + 0.6* I
  return z*z + z1
 }
@@ -22,54 +22,60 @@ fun f(z:Complex):Complex{ // определяем функцию, которая
 
 
   var z:Complex
+  var a: Double = -1.0
+  var b: Double = 0.0
+
   var flag:Boolean = true
-  var n = 1
+  var n = 0
   var sum:Complex
   var xs = arrayListOf<Double>()
   var ys = arrayListOf<Double>()
   var value = arrayListOf<Int>()
-  var tempMap: Array<Array<Int>> = Array(400, { Array(300,){0} })
   
 
 
-  for (i in 0..399)
+
+
+  while (a <=1)
   {
-   for (j in 0..299)
+
+
+   while (b <=1)
    {
-    z = complex((i-200)/100,(j-150)/75)
+    xs.add(a)
+    ys.add(b)
+    z = complex(a,b)
     n = 0
     flag = true
     while (flag==true)
     {
      sum = apply(::f,n)(z)
-     if(n == 150){//задаём лимит на n, чтобы цикл не ушёл в бесконечность при определённых значениях z
-      tempMap[i][j] = 150
+     if (n==150) {
+      value.add(150)
       flag = false
-     }
-     if(sum.mod > 2.0){// если модуль получившегося комплексного числа больше двойки, останавливаем цикл и запоминаем n
-      flag = false
-      tempMap[i][j]=n
+     }else
+     {
+      if(sum.mod > 2.0)
+      {
+       flag = false
+       value.add(n)
+      }
      }
      n++
     }
+    b += 0.01
    }
+   b = -1.0
+   a += 0.01
   }
 
 
 
-  for (i in 0..399)//создаём массивы координат точек и значений
-  {
-   for (j in 0..299){ 
-    xs.add((i-200.0)/100)
-    ys.add((j-150.0)/75)
-    value.add(tempMap[i][j])
-   }
-  }
 
   val data_3 = mapOf<String, Any>("xs" to xs,"ys" to ys , "value" to value)
-  val fig_3 = letsPlot(data_3) + 
-          
-          geomTile() {x = "xs"; y = "ys"; fill = "value";color = "value"} + labs(title = "Color map for Julia visualisation")
+  val fig_3 = letsPlot(data_3) +
+
+          geomTile( ) {x = "xs"; y = "ys"; fill = "value"; color = "value"} + labs(title = "Color map for Julia visualisation")
 
   ggsave(fig_3, "plot_3.png")
 
